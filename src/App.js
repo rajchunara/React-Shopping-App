42796454c43e738ae3f863e92d550aed9c74f78f
rajchunara <rajchunara777@gmail.com>
2018-10-23 15:24:52 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Finalorder from './components/Finalorder';
 import Home from './components/Home';
 
 class App extends Component {
-
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div className="App">
           <Route exact path='/' component={Home}/>
           <Route path='/Finalorder' component={Finalorder}/>
+          <Route component={() => (<div>404 Not found </div>)} />
         </div>
       </BrowserRouter>
     );
